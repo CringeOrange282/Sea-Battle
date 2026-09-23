4af92d97297645a1f13e86a718eae2bf9d14abd4
Лебедев В. <vladlebedev09042007@gmail.com>
2026-09-23 20:27:09 +0300

    Добавлен и покрыт тестами класс Ship

--- GoogleTests/PositionTests.cpp
+++ GoogleTests/PositionTests.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "Position.h"
+#include "Ship.h"
+
 
 TEST(PositionTest, ValidCoordinates) {
     Position p1(5, 3);
@@ -45,4 +47,83 @@ TEST(PositionTest, SettersAndGetters) {
     EXPECT_THROW(p.row(15), std::logic_error);
     EXPECT_THROW(p.col(0), std::logic_error);
     EXPECT_THROW(p.col('X'), std::logic_error);
+}
+
+
+
+#include "pch.h"
+#include "Ship.h"
+TEST(ShipTest, ValidConstructor) {
+    Position pos(1, 1);
+    Ship ship(3, pos, Horizontal);
+
+    EXPECT_EQ(ship.size(), 3);
+    EXPECT_EQ(ship.row(), 1);
+    EXPECT_EQ(ship.col(), 1);
+    EXPECT_EQ(ship.direction(), Horizontal);
+}
+
+TEST(ShipTest, InvalidConstructorSize) {
+    Position pos(1, 1);
+    EXPECT_THROW(Ship(0, pos, Horizontal), std::logic_error);
+    EXPECT_THROW(Ship(5, pos, Horizontal), std::logic_error);
+}
+
+TEST(ShipTest, InvalidConstructorOutOfBounds) {
+    Position pos1(1, 10);
+    EXPECT_THROW(Ship(2, pos1, Horizontal), std::logic_error);
+
+    Position pos2(9, 1);
+    EXPECT_THROW(Ship(3, pos2, Vertical), std::logic_error);
+}
+
+TEST(ShipTest, CharConstructorValid) {
+    Ship ship(4, 'V', 2, 'C');
+    EXPECT_EQ(ship.size(), 4);
+    EXPECT_EQ(ship.row(), 2);
+    EXPECT_EQ(ship.col(), 3);
+    EXPECT_EQ(ship.direction(), Vertical);
+}
+
+TEST(ShipTest, StringConstructorValid) {
+    Ship ship("3 H 4 B");
+    EXPECT_EQ(ship.size(), 3);
+    EXPECT_EQ(ship.row(), 4);
+    EXPECT_EQ(ship.col(), 2);
+    EXPECT_EQ(ship.direction(), Horizontal);
+}
+
+TEST(ShipTest, StringConstructorInvalid) {
+    EXPECT_THROW(Ship("5 H 1 A"), std::logic_error);
+    EXPECT_THROW(Ship("3 X 1 A"), std::logic_error);
+    EXPECT_THROW(Ship("4 H 2 J"), std::logic_error);
+    EXPECT_THROW(Ship("4 V 9 A"), std::logic_error);
+}
+
+TEST(ShipTest, SettersValid) {
+    Position pos(1, 1);
+    Ship ship(2, pos, Horizontal);
+
+    ship.size(3);
+    EXPECT_EQ(ship.size(), 3);
+
+    ship.row(5);
+    EXPECT_EQ(ship.row(), 5);
+
+    ship.col(4);
+    EXPECT_EQ(ship.col(), 4);
+
+    ship.direction(Vertical);
+    EXPECT_EQ(ship.direction(), Vertical);
+}
+
+TEST(ShipTest, SettersInvalid) {
+    Position pos(1, 1);
+    Ship ship(4, pos, Horizontal);
+
+    EXPECT_THROW(ship.size(5), std::logic_error);
+    EXPECT_THROW(ship.col(8), std::logic_error);
+
+    Ship ship_bottom(4, Position(9, 1), Horizontal);
+    EXPECT_THROW(ship_bottom.direction(Vertical), std::logic_error);
 }
